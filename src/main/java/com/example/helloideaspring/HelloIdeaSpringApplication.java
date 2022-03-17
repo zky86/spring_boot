8b77be2e7b27d322b0529dd90c8f16abf6d7d99b
zhengkangyuan <272495222@qq.com>
2022-03-17 16:34:38 +0800

    修改注释

--- src/main/java/com/example/helloideaspring/HelloIdeaSpringApplication.java
+++ src/main/java/com/example/helloideaspring/HelloIdeaSpringApplication.java
@@ -1,5 +1,4 @@
-package com.example.helloideaspring;//package com.example.helloideaspring;
-
+package com.example.helloideaspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
