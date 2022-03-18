1573b650d4dd33d8e32d3dafa245c668a60e27cd
zhengkangyuan <272495222@qq.com>
2022-03-18 11:37:28 +0800

    yml文件映射

--- src/main/java/com/example/helloideaspring/HelloController.java
+++ src/main/java/com/example/helloideaspring/HelloController.java
@@ -1,12 +1,32 @@
+//package com.example.helloideaspring;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//@Controller
+//public class HelloController {
+//    @ResponseBody
+//    @RequestMapping("/hello")
+//    public String hello() {
+//        return "Hello World!";
+//    }
+//}
+
 package com.example.helloideaspring;
+
+import com.example.helloideaspring.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HelloController {
-    @ResponseBody
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World!";
-    }
+  @Autowired
+  private Person person;
+
+  @ResponseBody
+  @RequestMapping("/hello")
+  public Person hello() {
+    return person;
+  }
 }
---
+++ src/main/java/com/example/helloideaspring/Person.java
@@ -0,0 +1,110 @@
+package com.example.helloideaspring;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 将配置文件中配置的每一个属性的值，映射到这个组件中
+ *
+ * @ConfigurationProperties：告诉 SpringBoot 将本类中的所有属性和配置文件中相关的配置进行绑定；
+ * prefix = "person"：配置文件中哪个下面的所有属性进行一一映射
+ * <p>
+ * 只有这个组件是容器中的组件，才能使用容器提供的@ConfigurationProperties功能；
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+  private String lastName;
+  private Integer age;
+  private Boolean boss;
+  private Date birth;
+  private Map<String, Object> maps;
+  private List<Object> lists;
+  private Dog dog;
+
+  public Person() {
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public Boolean getBoss() {
+    return boss;
+  }
+
+  public void setBoss(Boolean boss) {
+    this.boss = boss;
+  }
+
+  public Date getBirth() {
+    return birth;
+  }
+
+  public void setBirth(Date birth) {
+    this.birth = birth;
+  }
+
+  public Map<String, Object> getMaps() {
+    return maps;
+  }
+
+  public void setMaps(Map<String, Object> maps) {
+    this.maps = maps;
+  }
+
+  public List<Object> getLists() {
+    return lists;
+  }
+
+  public void setLists(List<Object> lists) {
+    this.lists = lists;
+  }
+
+  public Dog getDog() {
+    return dog;
+  }
+
+  public void setDog(Dog dog) {
+    this.dog = dog;
+  }
+
+  public Person(String lastName, Integer age, Boolean boss, Date birth, Map<String, Object> maps, List<Object> lists, Dog dog) {
+    this.lastName = lastName;
+    this.age = age;
+    this.boss = boss;
+    this.birth = birth;
+    this.maps = maps;
+    this.lists = lists;
+    this.dog = dog;
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+        "lastName='" + lastName + '\'' +
+        ", age=" + age +
+        ", boss=" + boss +
+        ", birth=" + birth +
+        ", maps=" + maps +
+        ", lists=" + lists +
+        ", dog=" + dog +
+        '}';
+  }
+}
